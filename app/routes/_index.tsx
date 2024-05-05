58e92016cf11cbfe013dcada0e4413ddba3839d0
Rashid Shamsudinov <rash2x@gmail.com>
2024-05-05 20:47:30 +0300

    polish

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,13 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Button } from "../components/ui/button";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { supabase } from "~/lib/supabase";
+import LogoPng from '~/assets/logo.png';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Yaswami - Найди свой ивент" },
+    { name: "description", content: "yaswami.com – платформа где каждый может создать свой ивент и пригласить других" },
   ];
 };
 
@@ -20,8 +21,13 @@ export default function Index() {
   console.log(data)
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <Button className="rounded-full w-40 h-40 text-xl">Meditate</Button>
+    <div className="h-dvh w-dvw flex flex-col gap-3 justify-center items-center">
+      <Button className="rounded-full w-40 h-40 p-1 text-xl hover:p-0 active:p-2 hover:scale-110 active:scale-90 transition-all" asChild>
+        <Link to="/events/create">
+          <img src={LogoPng} alt="" />
+        </Link>
+      </Button>
+      <div className="text-muted-foreground uppercase font-bold text-xs">Создать событие</div>
     </div>
   );
 }
