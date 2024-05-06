02aba35691f19388c24c138a4194e3bc7018ab4d
Rashid Shamsudinov <rash2x@gmail.com>
2024-05-06 15:49:06 +0300

    add share button

--- src/pages/events/view/page.tsx
+++ src/pages/events/view/page.tsx
@@ -1,9 +1,17 @@
 import { getEventById, getParticipateStatus, participateEvent, unparticipateEvent } from "../../../db/api";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useUserStore } from "~/db/userStore";
 import { Button } from "~/components/ui/button";
 import { Participants } from "./participants";
+
+
+function generateTelegramShareUrl(eventId?: string, text: string = '') {
+  if(!eventId) return '';
+
+  const encodedText = encodeURIComponent(text);
+  return `https://t.me/share/url?text=${encodedText}&url=https://t.me/yaswami_bot/join?startapp=event-${eventId}`;
+}
 
 export default function EventViewPage() {
   const { eventId } = useParams();
@@ -55,6 +63,8 @@ export default function EventViewPage() {
     }
   };
 
+  const shareUrl = generateTelegramShareUrl(eventId, 'Записывайся на событие! Используй ссылку ниже чтобы открыть информацию');
+
   return (
     eventData && (
       <div className="container py-8 space-y-2">
@@ -65,6 +75,10 @@ export default function EventViewPage() {
 
         {!isParticipated && <Button className="w-full" onClick={handleParticipate} disabled={participateMutation.isPending}>Принять участие</Button>}
         {isParticipated && <Button className="w-full" onClick={handleUnparticipate} disabled={unparticipateMutation.isPending} variant="destructive">Отменить участие</Button>}
+
+        <Button className="w-full" variant="secondary" asChild>
+          <Link to={shareUrl}>Поделиться</Link>
+        </Button>
       </div>
     )
   );
